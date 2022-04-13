6f8b641836f3b96be77dda7ef85ff6562ea4476e
Pedro Camejo <camejodiazpedro@gmail.com>
2022-04-13 17:37:43 +0100

    ommitNodeAction

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 
 try {
-    const nombre = await core.getInput('nombre');
+    const nombre = core.getInput('nombre');
     const tiempo = (new Date()).toLocaleTimeString();
 } catch {
     core.setFailed(error.message);
